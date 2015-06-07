e024faed784b16be3803e73f8520626b853baefc
kylemorton5770 <mikemorton91@gmail.com>
2015-06-07 14:18:24 -0400

    after login

--- public/app/controllers/userCtrl.js
+++ public/app/controllers/userCtrl.js
@@ -9,15 +9,23 @@ angular.module('userCtrl', [])
   vm.message = "Hello, World!";
 	vm.userName = "";
 	vm.loggedIn = false;
+	vm.tempUserName = "";
+	vm.welcomeMessage = "";
 
-	vm.login = function (name) {
-		vm.userName = name;
+	vm.login = function () {
+		console.log("login called with: " + vm.tempUserName);
+		vm.userName = vm.tempUserName;
+		console.log("new username: " + vm.userName);
 		vm.loggedIn = true;
+		vm.welcomeMessage = "Hello, " + vm.userName;
+		vm.tempUserName = "";
+		console.log("welcomeMessage: " + vm.welcomeMessage);
 	};
 
 	vm.logout = function () {
 			vm.userName = "";
 			vm.loggedIn = false;
+			vm.welcomeMessage = "";
 	}
 
 });
